c5abb8f42ee345cbe02dc76a0fd76491ecb7f081
thomw201 <thomwouda@gmail.com>
2017-01-18 01:41:23 +0100

    working multiplayer menu

--- NMPP/NMPP_NDS/source/MPMenu.cpp
+++ NMPP/NMPP_NDS/source/MPMenu.cpp
@@ -5,52 +5,54 @@ const int usedslots = 12;
 
 MPMenu::MPMenu(StateManager & manager) : GameState(manager)
 {
-	const char* const buttonSprite1[] = { "menu/singleplayer_1", "menu/multiplayer_1", "menu/options_1", "menu/singleplayerselected_1", "menu/multiplayerselected_1", "menu/optionsselected_1" };
-	const char* const buttonSprite2[] = { "menu/singleplayer_2", "menu/multiplayer_2", "menu/options_2", "menu/singleplayerselected_2", "menu/multiplayerselected_2", "menu/optionsselected_2" };
+	//NF_ResetSpriteBuffers();
+	//NF_InitSpriteBuffers();		// Initialize Sprite Buffers
+	//NF_InitSpriteSys(0);
+	//NF_InitSpriteSys(1);		// Initialize Bottom Screen SpriteSystem
+	const char* const buttonSprite1[] = { "menu/joinfriend_1", "menu/hostgame_1", "menu/back_1", "menu/joinfriendselected_1", "menu/hostgameselected_1", "menu/backselected_1" };
+	const char* const buttonSprite2[] = { "menu/joinfriend_2", "menu/hostgame_2", "menu/back_2", "menu/joinfriendselected_2", "menu/hostgameselected_2", "menu/backselected_2" };
 	//draw left side of all buttons
 	for (u16 i = 0; i < 3; i++)
 	{
-		NF_UnloadSpriteGfx(i);
-		NF_FreeSpriteGfx(1, i);
-		NF_UnloadSpritePal(i);
-		//NF_SPR256VRAM[1][i].inuse = false;
-		NF_LoadSpriteGfx(buttonSprite1[i], i, 64, 32);	// load left side of the button sprites
-		NF_LoadSpritePal(buttonSprite1[i], i);
-		NF_VramSpritePal(1, i, i);		// Load the Palette into VRAM 
-		NF_VramSpriteGfx(1, i, i, true);	// Load the Gfx into VRAM - transfer all Sprites
-		NF_CreateSprite(1, i, i, i, 64, 30 + (i * 50));
-		////draw right side of all buttons
-		//NF_LoadSpriteGfx(buttonSprite2[i], i + 3, 64, 32);	// load left side of the button sprites
-		//NF_LoadSpritePal(buttonSprite2[i], i + 3);
-		//NF_VramSpriteGfx(1, i + 3, i + 3, true);	// Load the Gfx into VRAM - transfer all Sprites
-		//NF_VramSpritePal(1, i + 3, i + 3);		// Load the Palette into VRAM 
-		//NF_CreateSprite(1, i + 3, i + 3, i + 3, 128, 30 + (i * 50));
+		int id = 12 + i;
+		NF_LoadSpriteGfx(buttonSprite1[i], id, 64, 32);	// load left side of the button sprites
+		NF_LoadSpritePal(buttonSprite1[i], id);
+		NF_VramSpritePal(1, id, i);		// Load the Palette into VRAM 
+		NF_VramSpriteGfx(1, id, id, true);	// Load the Gfx into VRAM - transfer all Sprites
+		NF_CreateSprite(1, id, id, i, 64, 30 + (i * 50));
+		//draw right side of all buttons
+
+		NF_LoadSpriteGfx(buttonSprite2[i], id + 3, 64, 32);	// load left side of the button sprites
+		NF_LoadSpritePal(buttonSprite2[i], id + 3);
+		NF_VramSpritePal(1, id + 3, i + 3);		// Load the Palette into VRAM 
+		NF_VramSpriteGfx(1, id + 3, id + 3, true);	// Load the Gfx into VRAM - transfer all Sprites
+		NF_CreateSprite(1, id + 3, id + 3, i + 3, 128, 30 + (i * 50));
 		////NF_ShowSprite(1, i+3, false); //hide selected sprite
 		////draw right side of all SELECTED buttons
-		//NF_LoadSpriteGfx(buttonSprite2[i + 3], i + 6, 64, 32);	// load left side of the button sprites
-		//NF_LoadSpritePal(buttonSprite2[i + 3], i + 6);
-		//NF_VramSpriteGfx(1, i + 6, i + 6, true);	// Load the Gfx into VRAM - transfer all Sprites
-		//NF_VramSpritePal(1, i + 6, i + 6);		// Load the Palette into VRAM 
-		//NF_CreateSprite(1, i + 6, i + 6, i + 6, 128, 30 + (i * 50));
-		////NF_ShowSprite(1, i + 6, false); //hide selected sprite
-		////draw left side of all SELECTED buttons
-		//NF_LoadSpriteGfx(buttonSprite1[i + 3], i + 9, 64, 32);	// load left side of the button sprites
-		//NF_LoadSpritePal(buttonSprite1[i + 3], i + 9);
-		//NF_VramSpriteGfx(1, i + 9, i + 9, true);	// Load the Gfx into VRAM - transfer all Sprites
-		//NF_VramSpritePal(1, i + 9, i + 9);		// Load the Palette into VRAM 
-		//NF_CreateSprite(1, i + 9, i + 9, i + 9, 64, 30 + (i * 50));
+		NF_LoadSpriteGfx(buttonSprite2[i + 3], id + 6, 64, 32);	// load left side of the button sprites
+		NF_LoadSpritePal(buttonSprite2[i + 3], id + 6);
+		NF_VramSpriteGfx(1, id + 6, id + 6, true);	// Load the Gfx into VRAM - transfer all Sprites
+		NF_VramSpritePal(1, id + 6, i + 6);		// Load the Palette into VRAM 
+		NF_CreateSprite(1, id + 6, id + 6, i + 6, 128, 30 + (i * 50));
+		//NF_ShowSprite(1, i + 6, false); //hide selected sprite
+		//draw left side of all SELECTED buttons
+		NF_LoadSpriteGfx(buttonSprite1[i + 3], id + 9, 64, 32);	// load left side of the button sprites
+		NF_LoadSpritePal(buttonSprite1[i + 3], id + 9);
+		NF_VramSpriteGfx(1, id + 9, id + 9, true);	// Load the Gfx into VRAM - transfer all Sprites
+		NF_VramSpritePal(1, id + 9, i + 9);		// Load the Palette into VRAM 
+		NF_CreateSprite(1, id + 9, id + 9, i + 9, 64, 30 + (i * 50));
 	}
 }
 
 
 MPMenu::~MPMenu()
 {
-	for (int i = 0; i < 12; i++)
+	for (u8 i = 0; i < 12; i++)
 	{
-		NF_DeleteSprite(1, i);
-		NF_FreeSpriteGfx(1, i);
 		NF_UnloadSpriteGfx(i);
 		NF_UnloadSpritePal(i);
+		NF_FreeSpriteGfx(1, i);
+		NF_DeleteSprite(1, i);
 	}
 	NF_ResetSpriteBuffers();
 }
@@ -100,19 +102,19 @@ void MPMenu::create()
 
 void MPMenu::navigate(signed int direction)
 {
-	//////show de-selected button
-	//NF_ShowSprite(1, usedslots+ selected, true); //left side of button
-	//NF_ShowSprite(1, usedslots+selected + 3, true); //right side of button
-	//selected = selected + direction;
-	//if (selected > 2)
-	//{
-	//	selected = 0;
-	//}
-	//if (selected < 0)
-	//{
-	//	selected = 2;
-	//}
-	//////hide deselected button
-	//NF_ShowSprite(1, usedslots+selected, false); //left side of button
-	//NF_ShowSprite(1, usedslots+selected + 3, false); //right side of button
+	////show de-selected button
+	NF_ShowSprite(1, usedslots+ selected, true); //left side of button
+	NF_ShowSprite(1, usedslots+selected + 3, true); //right side of button
+	selected = selected + direction;
+	if (selected > 2)
+	{
+		selected = 0;
+	}
+	if (selected < 0)
+	{
+		selected = 2;
+	}
+	////hide deselected button
+	NF_ShowSprite(1, usedslots+selected, false); //left side of button
+	NF_ShowSprite(1, usedslots+selected + 3, false); //right side of button
 }
